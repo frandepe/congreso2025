7cf5860272b6390a39f30ed696a24861cfafea71
Finanflix <projectfinanflix@gmail.com>
2025-03-19 13:34:42 -0300

    primera deploy 19 m

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -6,7 +6,12 @@ import React from "react";
 export const Home = () => {
   return (
     <div>
-      <div className="bg-darkGreen text-white p-4">
+      <div className="flex items-center justify-center h-screen bg-gray-100">
+        <h1 className="text-4xl font-bold text-gray-700">
+          Página en construcción
+        </h1>
+      </div>
+      {/* <div className="bg-darkGreen text-white p-4">
         <h1 className="text-3xl">Este es un título con fondo verde oscuro</h1>
       </div>
 
@@ -23,7 +28,7 @@ export const Home = () => {
       <div className="bg-lightGreen text-white p-4">
         <h1 className="text-3xl">Este es un título con fondo verde claro</h1>
       </div>
-      <Button className="bg-primary">a ver</Button>
+      <Button className="bg-primary">a ver</Button> */}
       <Countdown />
     </div>
   );
