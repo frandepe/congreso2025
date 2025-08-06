2d581827b78f4da7940fdd9ee22fb5f20deb0800
asd <frandepaulo@yahoo.com.ar>
2025-08-06 11:52:56 -0300

    prueba apoyos

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,53 +1,37 @@
+import { Agradecimientos } from "@/components/Agradecimientos/Agradecimientos";
 import BannerPrincipal from "@/components/Banners/BannerPrincipal";
 import { Comite } from "@/components/Comite/Comite";
 import Countdown from "@/components/Countdown/Countdown";
 import { DescImgs } from "@/components/DescImgs/DescImgs";
-import { Button } from "@/components/ui/button";
-import { StickyScroll } from "@/components/ui/sticky-scroll-reveal";
+import { Projector, Sandwich } from "lucide-react";
 import React from "react";
 
-// const content = [
-//   {
-//     title: "Título 1",
-//     description:
-//       "Descripción 1Descripción 1Descripción 1Descripción 1Descripción 1Descripción 1Descripción 1Descripción 1Descripción 1Descripción 1Descripción 1Descripción 1Descripción 1Descripción 1Descripción 1Descripción 1Descripción 1Descripción 1Descripción 1Descripción 1Descripción 1Descripción 1Descripción 1Descripción 1",
-//     content: "Contenido 1",
-//   },
-//   {
-//     title: "Título 2",
-//     description:
-//       "Descripción 2 Descripción 1Descripción 1Descripción 1Descripción 1Descripción 1Descripción 1Descripción 1Descripción 1Descripción 1Descripción 1Descripción 1Descripción 1Descripción 1Descripción 1Descripción 1Descripción 1Descripción 1Descripción 1Descripción 1Descripción 1",
-//     content: "Contenido 2",
-//   },
-//   {
-//     title: "Título 2",
-//     description:
-//       "Descripción 2 Descripción 1Descripción 1Descripción 1Descripción 1Descripción 1Descripción 1Descripción 1Descripción 1Descripción 1Descripción 1Descripción 1Descripción 1Descripción 1Descripción 1Descripción 1Descripción 1Descripción 1Descripción 1Descripción 1Descripción 1",
-//     content: "Contenido 2",
-//   },
-// ];
-// https://ar.pinterest.com/pin/921760248723153667/visual-search/?surfaceType=flashlight&rs=pin
-// https://ar.pinterest.com/pin/879468633471773463/visual-search/?surfaceType=flashlight&rs=search
-{
-  /* <div className="bg-darkGreen text-white p-4">
-        <h1 className="text-3xl">Este es un título con fondo verde oscuro</h1>
-      </div>
-
-      <div className="bg-orange text-white p-4">
-        <h1 className="text-3xl">Este es un título con fondo naranja</h1>
-      </div>
-
-      <div className="bg-redOrange text-white p-4">
-        <h1 className="text-3xl">
-          Este es un título con fondo rojo anaranjado
-        </h1>
-      </div>
-
-      <div className="bg-lightGreen text-white p-4">
-        <h1 className="text-3xl">Este es un título con fondo verde claro</h1>
-      </div>
-      <Button className="bg-primary">a ver</Button> */
-}
+const features= [
+  {
+    id: 1,
+    icon: Projector,
+    title: "Ep Eventos",
+    description:
+      "Este congreso contará con sonido, iluminación y pantalla de EP EVENTOS",
+    image: "/assets/ep-eventos.jpeg",
+  },
+  {
+    id: 2,
+    icon: Sandwich,
+    title: "Food truck Los Cuscus",
+    description:
+      "La mejor calidad y servicio en comida rápida la tenemos con nosotros",
+    image: "/assets/foot-truck2.jpg",
+  },
+  // {
+  //   id: 3,
+  //   icon: Brain,
+  //   title: "AI-Powered Learning",
+  //   description:
+  //     "Experience personalized, AI-driven learning tailored for BCA, BBA, and other students.",
+  //   image: "https://bcalabs.org/companions_group_2.jpg",
+  // },
+];
 export const Home = () => {
   const testimonials = [
     {
@@ -83,6 +67,13 @@ export const Home = () => {
         <div className="mt-4 flex justify-center px-12">
           <Comite testimonials={testimonials} />
         </div>
+      </div>
+      <div className="mt-20">
+        <Agradecimientos primaryColor="sky-500"
+        progressGradientLight="bg-gradient-to-r from-secondary/70 to-secondary"
+        progressGradientDark="bg-gradient-to-r from-secondary/70 to-secondary"
+        features={features}
+        />
       </div>
     </div>
   );
